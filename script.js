fb9840dd3e2b6a0884e6e006f88b7cf32b6d571d
Jagocoding234 <azumazum07@gmail.com>
2024-07-23 22:32:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// toogle itu yang tadinya ada jadi tidak ada dan yang tidak ada jadi ada
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// click di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
